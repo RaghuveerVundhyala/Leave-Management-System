1890abc9e7e69e31e7bd0b35a541ddf69502474d
RaghuveerVundhyala <raghuvindhyala@gmail.com>
2024-03-19 00:04:21 -0400

    removed vercel changes

--- LeaveMgmt-Django/src/hrsuit/settings.py
+++ LeaveMgmt-Django/src/hrsuit/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'This Is Secret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 # Email Settings
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
